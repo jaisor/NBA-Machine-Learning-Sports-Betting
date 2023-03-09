825ca241ca053404b93ed0a6d4725150a176a59e
jaisor <jaisor@gmail.com>
2023-03-09 08:51:47 -0800

    start on JSON output

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import argparse
 from colorama import Fore, Style
 import pandas as pd
 import tensorflow as tf
+from io import StringIO
+import json
 from datetime import datetime
 from src.Predict import NN_Runner, XGBoost_Runner
 from src.Utils.Dictionaries import team_index_current
@@ -60,28 +62,53 @@ def createTodaysGames(games, df, odds):
 
 
 def main():
+    jsonDict = {}
+
+    if args.format == 'json':
+        print("TEXT OUTPUT")
+
     odds = None
     date = None
     if not args.date:
         date = datetime.today().strftime("%Y-%m-%d")
     else:
         date = args.date
+    
+    jsonDict['date'] = datetime.strptime(args.date, '%Y-%m-%d').date().isoformat()
+    jsonDict['sport'] = args.sport
+
     if args.odds:
+        jsonDict['odds_source'] = args.odds
         odds = SbrOddsProvider(sportsbook=args.odds, date=args.date, sport=args.sport).get_odds()
         games = create_todays_games_from_odds(odds)
         if len(games) == 0:
             print("No games found.")
+            jsonDict['status'] = 'error'
+            jsonDict['error'] = 'no games found'
             return
         if((games[0][0]+':'+games[0][1]) not in list(odds.keys())):
             print(games[0][0]+':'+games[0][1])
             print(Fore.RED, "--------------Games list not up to date for todays games!!! Scraping disabled until list is updated.--------------")
             print(Style.RESET_ALL)
+            jsonDict['warnings'].append('Games list not up to date for todays games. Scraping disabled until list is updated')
             odds = None
         else:
             print(f"------------------{args.odds} odds data------------------")
+            oddsList = []
             for g in odds.keys():
                 home_team, away_team = g.split(":")
                 print(f"{away_team} ({odds[g][away_team]['money_line_odds']}) @ {home_team} ({odds[g][home_team]['money_line_odds']})")
+                oddsList.append({
+                    'away_team': {
+                        'name': away_team,
+                        'money_line_odds': odds[g][away_team]['money_line_odds'],
+                    },
+                    'home_team': {
+                        'name': home_team,
+                        'money_line_odds': odds[g][home_team]['money_line_odds'],
+                    }
+                })
+            jsonDict['odds_data'] = oddsList
     else:
         data = get_todays_games_json(todays_games_url)
         games = create_todays_games(data)
@@ -106,6 +133,8 @@ def main():
         NN_Runner.nn_runner(data, todays_games_uo, frame_ml, games, home_team_odds, away_team_odds)
         print("-------------------------------------------------------")
 
+    if args.format == 'json':
+        print(json.dumps(jsonDict, indent=4))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Model to Run')
@@ -115,5 +144,6 @@ if __name__ == "__main__":
     parser.add_argument('-odds', help='Sportsbook to fetch from. (fanduel, draftkings, betmgm, pointsbet, caesars, wynn, bet_rivers_ny')
     parser.add_argument('-date', help='Date of the games to fetch. (optional, omit for today)')
     parser.add_argument('-sport', help='Sport. (NBA, NFL, NHL, MLB, NCAAB)', default="NBA")
+    parser.add_argument('-format', help='Output format. (text, json)', default="text")
     args = parser.parse_args()
     main()
